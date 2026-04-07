2c655a2d08995f89bf41f894229ca30ac071c2ae
Philip Dushie <philip.dushie@gmail.com>
2026-04-07 09:53:47 +0000

    Fixes json body limit of 100kb

--- api-server.js
+++ api-server.js
@@ -9,7 +9,8 @@ const { withFileLock, atomicWrite } = require('./lock');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 const UPLOADED_LOG = path.join(process.env.EXCEL_FOLDER_PATH || '.', '.uploaded.json');
